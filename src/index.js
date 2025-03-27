34bcbd583fdc4f65d698c0075ab22979175a6dc4
Leslie Kogi <lesliekogi@gmail.com>
2025-03-27 18:41:31 +0300

    fetches categories and places them

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
-console.log("Page acess")
+document.addEventListener("DOMContentLoaded",()=>{
+ /*fetching categories */
+    fetch('http://localhost:3000/categories')
+    .then (resp=>resp.json())
+    .then (data=>placingCategories(data))
+    .catch(() => {
+            return console.log("Error when fetching categories");
+        })
+
+    function placingCategories(categories){
+        const categoryMainConatiner=document.querySelector('#Activities_planned')
+        categoryMainConatiner.innerHTML=''
+        categories.forEach(category => {
+            const categoryContainer=document.createElement('div')
+            categoryContainer.className='category-item';
+            categoryContainer.textContent=category.name;
+            categoryMainConatiner.appendChild(categoryContainer);
+            
+        });
+
+    }
+
+    
+})
